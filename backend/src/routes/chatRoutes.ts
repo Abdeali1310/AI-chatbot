3f77724342316a22d73f3cd2d9cd95c28241d471
unknown <abdealidahodwala163@gmail.com>
2024-12-02 23:57:59 +0530

    Retrive all chats

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,8 +1,10 @@
 import { verifyToken } from "../utils/token-manager";
-const { generateChatCompletion } = require("../controllers/chatController");
+const { generateChatCompletion,getAllChats } = require("../controllers/chatController");
 const express = require("express");
 const chatRouter = express.Router();
 
 chatRouter.post("/new", verifyToken, generateChatCompletion);
+chatRouter.get("/all-chats", verifyToken, getAllChats);
+
 
 module.exports = chatRouter;
